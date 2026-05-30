5671927917115703a5eaf99b06c80a41a32f3ee3
David <david.code999@gmail.com>
2026-05-30 21:44:58 +0200

    remove API key

--- lib/firebase/config.ts
+++ lib/firebase/config.ts
@@ -3,12 +3,12 @@ import { initializeFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey:            process.env.NEXT_PUBLIC_FIREBASE_API_KEY            ?? "AIzaSyDsd7NPGGdz4Cd1-yXQRr2xi06iXS6KVfM",
-  authDomain:        process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN        ?? "gcclouange.firebaseapp.com",
-  projectId:         process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID         ?? "gcclouange",
-  storageBucket:     process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET     ?? "gcclouange.firebasestorage.app",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID ?? "189043408205",
-  appId:             process.env.NEXT_PUBLIC_FIREBASE_APP_ID             ?? "1:189043408205:web:54763a01f4216cede8cb04",
+  apiKey:            process.env.NEXT_PUBLIC_FIREBASE_API_KEY            ,
+  authDomain:        process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN        ,
+  projectId:         process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID         ,
+  storageBucket:     process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET     ,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID ,
+  appId:             process.env.NEXT_PUBLIC_FIREBASE_APP_ID             ,
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
